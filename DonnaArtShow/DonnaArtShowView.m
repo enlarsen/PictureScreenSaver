5a9d71ec102e41fdd6983c4ac4b5b0b5db3177e9
Erik Larsen <erikla@Eriks-iMac.local>
2015-03-01 20:45:12 -0800

    Reads/writes settings to defaults, UI added to change defaults, auto layout added to UI.

--- DonnaArtShow/DonnaArtShowView.m
+++ DonnaArtShow/DonnaArtShowView.m
@@ -9,6 +9,9 @@
 
 #import "DonnaArtShowView.h"
 
+static NSString * const pictureDirectoryKey = @"pictureDirectory";
+static NSString * const pictureChangeIntervalKey = @"pictureIntervalChange";
+
 @interface DonnaArtShowView()
 
 @property (nonatomic) NSTimeInterval changeTimeInterval;
@@ -17,20 +20,31 @@
 @property (strong, nonatomic) NSArray *files;
 @property (nonatomic) int currentIndex;
 @property (strong, nonatomic) NSImage *currentImage;
-
+@property (strong, nonatomic) NSString *bundleIdentifier;
+@property (strong, nonatomic) ScreenSaverDefaults *defaults;
 @end
 
 @implementation DonnaArtShowView
 
 #pragma mark - Properties
 
-- (NSString *)path
+- (NSString *)bundleIdentifier
 {
-    if(!_path)
+    if(!_bundleIdentifier)
     {
-        _path = @"/Users/erikla/Desktop/screensaver";
+        _bundleIdentifier = [[NSBundle bundleForClass:[self class]] bundleIdentifier];
     }
-    return _path;
+    return _bundleIdentifier;
+
+}
+
+- (ScreenSaverDefaults *)defaults
+{
+    if(!_defaults)
+    {
+        _defaults = [ScreenSaverDefaults defaultsForModuleWithName:self.bundleIdentifier];
+    }
+    return _defaults;
 }
 
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
@@ -38,10 +52,18 @@
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self)
     {
-        [self setAnimationTimeInterval:5.0];
-        self.changeTimeInterval = 1.0; //20.0 * 60.0;
+
+        [self.defaults registerDefaults:[DonnaArtShowView defaultsDictionary]];
+        self.animationTimeInterval = 5.0;
+
+        self.changeTimeInterval = [self.defaults doubleForKey:pictureChangeIntervalKey];
+
+        self.path = [[self.defaults
+                     stringForKey:pictureDirectoryKey] stringByExpandingTildeInPath];
+        //        self.changeTimeInterval = 5.0; //20.0 * 60.0;
         self.currentTimeInterval = self.changeTimeInterval;
         self.currentIndex = 0;
+
     }
     return self;
 }
@@ -152,31 +174,80 @@
 
 - (BOOL)hasConfigureSheet
 {
-    return NO;
+    return YES;
 }
 
 - (NSWindow*)configureSheet
 {
+    BOOL loadedBundle = NO;
     if(!self.configureWindow)
     {
         NSBundle *bundle = [NSBundle bundleForClass:[self class]];
-        BOOL result = [bundle loadNibNamed:@"ConfigurationSheet" owner:self topLevelObjects:nil];
-        [self.pictureChangeIntervalComboBox addItemsWithObjectValues:[DonnaArtShowView comboBoxToInterval].allKeys];
+        loadedBundle = [bundle loadNibNamed:@"ConfigurationSheet" owner:self topLevelObjects:nil];
     }
+
+    for(NSString *key in [DonnaArtShowView comboBoxToInterval].allKeys)
+    {
+        if(loadedBundle == YES) // Only add the items if we just loaded the window
+        {
+            [self.pictureChangeIntervalComboBox addItemWithObjectValue:key];
+        }
+        if(fabs(self.changeTimeInterval -
+                [[DonnaArtShowView comboBoxToInterval][key] doubleValue]) < 0.01)
+        {
+            [self.pictureChangeIntervalComboBox selectItemWithObjectValue:key];
+        }
+    }
+//    [self.pictureChangeIntervalComboBox addItemsWithObjectValues:[DonnaArtShowView comboBoxToInterval].allKeys];
+
+    self.picturesFolderTextField.stringValue = self.path;
+
+
     return self.configureWindow;
 
 }
 - (IBAction)configureOK:(NSButton *)sender
 {
-    
+    if(![self.picturesFolderTextField.stringValue isEqualToString:self.path])
+    {
+        self.path = self.picturesFolderTextField.stringValue;
+        [self.defaults setObject:self.path forKey:pictureDirectoryKey];
+        [self.defaults synchronize];
+    }
+
+    NSNumber *selectedInterval = [DonnaArtShowView comboBoxToInterval].allValues[self.pictureChangeIntervalComboBox.indexOfSelectedItem];
+    if(fabs([selectedInterval doubleValue] - self.changeTimeInterval) > 0.01)
+    {
+        self.changeTimeInterval = [selectedInterval doubleValue];
+        [self.defaults setObject:@(self.changeTimeInterval) forKey:pictureChangeIntervalKey];
+        [self.defaults synchronize];
+    }
+
+    [NSApp endSheet:self.configureWindow];
 }
 
 - (IBAction)configureCancel:(NSButton *)sender
 {
-
+    [NSApp endSheet:self.configureWindow];
 }
 - (IBAction)configureChooseFolder:(NSButton *)sender
 {
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    openPanel.allowsMultipleSelection = NO;
+    openPanel.canChooseFiles = NO;
+    openPanel.canChooseDirectories = YES;
+    openPanel.canCreateDirectories = NO;
+    openPanel.prompt = @"Choose";
+
+    [openPanel beginSheetModalForWindow:self.configureWindow
+                      completionHandler:^(NSInteger result)
+    {
+        if(result == NSFileHandlingPanelOKButton)
+        {
+            NSURL *selectedDirectoryURL = [openPanel URL];
+            self.picturesFolderTextField.stringValue = selectedDirectoryURL.path;
+        }
+    }];
 
 }
 
@@ -190,6 +261,13 @@
              @"4 hours": @(60 * 60 * 4),
              @"8 hours": @(60 * 60 * 8),
              @"Day": @(60 *60 * 24)};
+}
+
++ (NSDictionary *)defaultsDictionary
+{
+    // TODO: must be able to expand tildes in paths for this to work
+    return @{pictureDirectoryKey: @"~/Desktop",
+             pictureChangeIntervalKey: @5.0};
 }
 
 @end
